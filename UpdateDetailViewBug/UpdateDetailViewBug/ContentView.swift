bb5f655a39a8a1927d7c09b93ab69714b361d799
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2024-11-26 14:04:39 +0000

    Add project

---
+++ UpdateDetailViewBug/UpdateDetailViewBug/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  UpdateDetailViewBug
+//
+//  Created by Suhayl Ahmed on 26/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundStyle(.tint)
+			Text("Hello, world!")
+		}
+		.padding()
+	}
+}
---
+++ UpdateDetailViewBug/UpdateDetailViewBug/UpdateDetailViewBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpdateDetailViewBugApp.swift
+//  UpdateDetailViewBug
+//
+//  Created by Suhayl Ahmed on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct UpdateDetailViewBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
